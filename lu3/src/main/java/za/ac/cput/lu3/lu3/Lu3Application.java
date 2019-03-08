10f3b77c9075f5ce26cfd60ebaaa41e5f3be5e28
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2019-03-08 16:58:18 +0200

    beta Commit with Spring 11

--- lu3/src/main/java/za/ac/cput/lu3/lu3/Lu3Application.java
+++ lu3/src/main/java/za/ac/cput/lu3/lu3/Lu3Application.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lu3Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Lu3Application.class, args);
-        System.out.println("Lungiesaaa");
-
+        System.out.println("Lungisaaa");
     }
-
 }
